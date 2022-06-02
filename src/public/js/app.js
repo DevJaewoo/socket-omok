862eee7a27a97a3b0571769fcb74a6d35a63c7bf
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-06-02 21:57:30 +0900

    방 만들기 / 입장하기 작성중

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,52 @@
 const socket = io();
 
-ReactDOM.render(<Counter />, document.getElementById("root"));
+function handleNewRoom(event) {
+  event.preventDefault();
+  const name = event.target.roomname.value;
+  if (name.length == 0) return;
+  socket.emit("new_room", name);
+}
+
+function handleEnterRoom(event, name) {
+  event.preventDefault();
+}
+
+const NewRoom = () => {
+  return (
+    <form onSubmit={handleNewRoom}>
+      <input type="text" name="roomname" placeholder="방 이름"></input>
+      <button>방 만들기</button>
+    </form>
+  );
+};
+
+const RoomItem = (name) => {
+  return (
+    <li key={name} class="room-list__item">
+      <p>{name}</p>
+      <button class="room-list__enter">입장하기</button>
+    </li>
+  );
+};
+
+const RoomList = () => {
+  const [roomList, setRoomList] = React.useState([]);
+  const listItems = roomList.map(RoomItem);
+
+  socket.on("room_list", (list) => {
+    setRoomList(list);
+  });
+
+  return <ul class="room-list">{listItems}</ul>;
+};
+
+const WaitingRoom = () => {
+  return (
+    <>
+      <NewRoom />
+      <RoomList />
+    </>
+  );
+};
+
+ReactDOM.render(<WaitingRoom />, document.getElementById("root"));
--- src/server.js
+++ src/server.js
@@ -17,10 +17,36 @@ app.get("/", (req, res) => {
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
+///////////////////////////////////////////////////////////
+
+const publicRoom = [];
+
 wsServer.on("connection", (socket) => {
   socket.onAny((event) => {
     console.log(`Socket event: ${event}`);
   });
+
+  socket.on("new_room", (name) => {
+    //Socket은 ID와 같은 Room을 Default로 갖고 있음
+    if (socket.rooms.size > 1) {
+      console.log(`socket ${socket.id} is already in room.`);
+      console.log(socket.rooms);
+      return;
+    }
+
+    if (publicRoom.includes(name)) {
+      console.log(`Room name ${name} already exists.`);
+      return;
+    }
+
+    publicRoom.push(name);
+    wsServer.sockets.emit("room_list", publicRoom);
+
+    socket.join(name);
+    socket.emit("room_enter", name);
+  });
+
+  socket.emit("room_list", publicRoom);
 });
 
 httpServer.listen(port, () => {
