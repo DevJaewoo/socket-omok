a725214c5d9059eed14eb1b1701aee1273bad329
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-06-03 12:38:54 +0900

    방 만들기, 참가 기능 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,30 +1,56 @@
 const socket = io();
 
+socket.onAny((event) => {
+  console.log(`Socket event: ${event}`);
+});
+
+socket.on("error", (message) => {
+  alert(message);
+});
+
 function handleNewRoom(event) {
   event.preventDefault();
   const name = event.target.roomname.value;
+  event.target.roomname.value = "";
   if (name.length == 0) return;
   socket.emit("new_room", name);
 }
 
-function handleEnterRoom(event, name) {
+function handleEnterRoom(name) {
   event.preventDefault();
+  socket.emit("enter_room", name);
 }
+
+const Header = () => {
+  return <h1 className="title">Socket Omok</h1>;
+};
 
 const NewRoom = () => {
   return (
-    <form onSubmit={handleNewRoom}>
-      <input type="text" name="roomname" placeholder="방 이름"></input>
-      <button>방 만들기</button>
-    </form>
+    <div className="newroom">
+      <form className="newroom__form" onSubmit={handleNewRoom}>
+        <input
+          className="newroom__input"
+          type="text"
+          name="roomname"
+          placeholder="방 이름"
+        ></input>
+        <button className="newroom__submit">방 만들기</button>
+      </form>
+    </div>
   );
 };
 
 const RoomItem = (name) => {
   return (
-    <li key={name} class="room-list__item">
-      <p>{name}</p>
-      <button class="room-list__enter">입장하기</button>
+    <li key={name} className="room-list__item">
+      <p className="room-list__name">{name}</p>
+      <button
+        className="room-list__enter"
+        onClick={() => handleEnterRoom(name)}
+      >
+        입장하기
+      </button>
     </li>
   );
 };
@@ -33,16 +59,22 @@ const RoomList = () => {
   const [roomList, setRoomList] = React.useState([]);
   const listItems = roomList.map(RoomItem);
 
-  socket.on("room_list", (list) => {
+  socket.on("room_change", (list) => {
     setRoomList(list);
   });
 
-  return <ul class="room-list">{listItems}</ul>;
+  return (
+    <div className="room-list">
+      <h3>방 목록</h3>
+      <ul className="room-list__container">{listItems}</ul>
+    </div>
+  );
 };
 
 const WaitingRoom = () => {
   return (
     <>
+      <Header />
       <NewRoom />
       <RoomList />
     </>
--- src/server.js
+++ src/server.js
@@ -19,34 +19,74 @@ const wsServer = new Server(httpServer);
 
 ///////////////////////////////////////////////////////////
 
-const publicRoom = [];
+let publicRoom = [];
+function countRoom(name) {
+  return wsServer.sockets.adapter.rooms.get(name).size;
+}
+
+function leaveRoom(socket, name) {
+  if (countRoom(name) == 1) {
+    publicRoom = publicRoom.filter((value) => value != name);
+    wsServer.sockets.emit("room_change", publicRoom);
+  }
+  socket.leave(name);
+}
 
 wsServer.on("connection", (socket) => {
   socket.onAny((event) => {
     console.log(`Socket event: ${event}`);
   });
 
+  //방 만들기
   socket.on("new_room", (name) => {
+    console.log(`Socket ${socket.id} is creating room ${name}.`);
+
     //Socket은 ID와 같은 Room을 Default로 갖고 있음
     if (socket.rooms.size > 1) {
       console.log(`socket ${socket.id} is already in room.`);
       console.log(socket.rooms);
+      socket.emit("error", "이미 다른 방에 참가중입니다.");
       return;
     }
 
+    //동일한 방이 존재할 경우
     if (publicRoom.includes(name)) {
       console.log(`Room name ${name} already exists.`);
+      socket.emit("error", "동일한 방이 이미 존재합니다.");
       return;
     }
 
     publicRoom.push(name);
-    wsServer.sockets.emit("room_list", publicRoom);
+    wsServer.sockets.emit("room_change", publicRoom);
 
     socket.join(name);
-    socket.emit("room_enter", name);
+    socket.emit("room_info", name);
   });
 
-  socket.emit("room_list", publicRoom);
+  //기존 방 참가
+  socket.on("enter_room", (name) => {
+    console.log(`Socket ${socket.id} is entering room ${name}.`);
+
+    if (socket.rooms.size > 1) {
+      console.log(`socket ${socket.id} is already in room.`);
+      console.log(socket.rooms);
+      socket.emit("error", "이미 다른 방에 참가중입니다.");
+      return;
+    }
+
+    socket.join(name);
+    socket.emit("room_info", name);
+  });
+
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((name) => {
+      if (name == socket.id) return;
+      leaveRoom(socket, name);
+    });
+  });
+
+  //최초 방 리스트 불러오기
+  socket.emit("room_change", publicRoom);
 });
 
 httpServer.listen(port, () => {
